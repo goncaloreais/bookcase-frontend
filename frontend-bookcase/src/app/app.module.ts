dcf2935ce882035de92695a5a1fedf7755dfc38e
Gonçalo Reais <goncalorsd@gmail.com>
2019-02-06 20:09:29 +0000

    adds material and a new top-menu

--- frontend-bookcase/src/app/app.module.ts
+++ frontend-bookcase/src/app/app.module.ts
@@ -5,6 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopMenuComponent } from './top-menu/top-menu.component';
 
+// material imports
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +17,9 @@ import { TopMenuComponent } from './top-menu/top-menu.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
